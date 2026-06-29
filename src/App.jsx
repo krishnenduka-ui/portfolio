f6c075717140b4ec937bc91fb32d8d7f3c9d13a5
krishnendu <krishnendu.ka@gmail.com>
2026-06-29 18:30:56 +0530

    Fix App.css import issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './app.css'
+import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
